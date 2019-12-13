59d093634f11e5ec458e6dede8393db47a51bd40
Engin Beyazgül <beyazgulengin@gmail.com>
2019-12-13 17:40:38 +0300

    Update index.php

--- OOP_Class/kurucu_yıkıcı_metodlar/index.php
+++ OOP_Class/kurucu_yıkıcı_metodlar/index.php
@@ -1,20 +1,20 @@
 <?php
 class DB
 {
-    private $db;
-    public function __construct($host,$kadi,$sifre)
+    private $db;//değişken oluşturduk
+    public function __construct($host,$kadi,$sifre)//yolladığımız ayarları değişkenlere aldık
     {
-        $this->db = new PDO('mysql:host='.$host,$kadi,$sifre);
+        $db = new PDO('mysql:host='.$host,$kadi,$sifre);//veritabanına bağlandık
     }
     public function getData()
     {
-        echo'db den gelen data';
+        echo'db den gelen data';//verileri  gösterdik
     }
     public function __destruct()
     {
         $this->db = null;
     }
 }
-$db = new DB('localhost','root','');
-echo $db->getData();
+$db = new DB('localhost','root','');//DB clasını başlatıp ayarları yolladık
+echo $db->getData();//dataları gösterdik
 ?>
